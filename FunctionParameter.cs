ce9b7e1c850271d735834c5fd4b2293cd46730b6
Shrikant Pawar <shrikantpawar703@gmail.com>
2024-08-27 02:15:37 -0700

    Add project files.

---
+++ FunctionParameter.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parameter
+{
+	internal class FunctionParameter
+	{
+		public int Num1 { get; set; }
+		public int Num2 { get; set; }
+
+		#region Simple And Input Parameter
+
+		public void Addition(int A, int B)
+		{
+			int Result = A + B;
+
+			Console.WriteLine($"Addition Of {A} & {B} = {Result}");
+		}
+
+		#endregion
+
+		#region Complex And Input Parameter
+		public void Substratction(FunctionParameter parameter)
+		{
+			int Result = parameter.Num1 - parameter.Num2;
+			Console.WriteLine($"Substraction Of {parameter.Num1} & {parameter.Num2} = {Result}");
+		}
+
+		#endregion
+
+		#region Output Parameter
+
+		public void Multiplication(int a, int b, out int Result)
+		{
+			Result = a * b;
+			Console.WriteLine($"Multiplication Of {a} & {b} = {Result}");
+		}
+
+		#endregion
+
+		#region Input-Output Parameter
+
+		public void Division(int x, int y, ref int Result1)
+		{
+			int Result = Result1 + x / y;
+			Console.WriteLine($"Division Of {Result1} + {x} / {y}   = {Result}");
+		}
+
+		#endregion
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿
+
+
+using Parameter;
+using System.Transactions;
+
+class Program
+{
+	public static void Main()
+	{
+		// Simple And Input Parameter
+		FunctionParameter functionParameter = new FunctionParameter();
+		functionParameter.Addition(30, 20); 
+
+
+		// Complex And Input Parameter
+		FunctionParameter functionParameter1 = new FunctionParameter();
+		functionParameter1.Num1 = 30;
+		functionParameter1.Num2 = 20;
+		
+		FunctionParameter functionParameter2 = new FunctionParameter();
+		functionParameter.Substratction(functionParameter1);
+
+		// Output Parameter
+		int Result;
+		FunctionParameter functionParameter3 = new FunctionParameter();
+		functionParameter3.Multiplication(5, 2, out Result);
+
+		// Input-Output Parameter 
+		int Result1=11;
+		FunctionParameter functionParameter4 = new FunctionParameter();
+		functionParameter4.Division(10,5,ref Result1);
+	}
+}
